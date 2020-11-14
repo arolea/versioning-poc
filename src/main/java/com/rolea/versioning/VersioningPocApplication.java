d54523dc29f62f6d3046f8dde579e5cc04e26548
Alex Rolea <alexrolea93@gmail.com>
2020-11-14 09:30:10 +0200

    Initial commit

---
+++ src/main/java/com/rolea/versioning/VersioningPocApplication.java
@@ -0,0 +1,13 @@
+package com.rolea.versioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersioningPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersioningPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rolea/versioning/VersioningPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolea.versioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersioningPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
