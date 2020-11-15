8599d551997800894c1daae080566ee6666237f1
Alex Rolea <alexrolea93@gmail.com>
2020-11-15 11:37:13 +0200

    Small adjsutments to object mapper

--- src/main/java/com/rolea/versioning/config/AppConfig.java
+++ src/main/java/com/rolea/versioning/config/AppConfig.java
@@ -1,6 +1,9 @@
 package com.rolea.versioning.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,12 @@ public class AppConfig {
 
     @Bean
     public ObjectMapper objectMapper(){
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+        objectMapper.configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
         return new ObjectMapper();
     }
 
